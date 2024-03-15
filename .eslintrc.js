037e884f061ed69992dec90edc137957b0ab572e
Karlito14 <leiroz26@hotmail.com>
2024-03-15 22:39:44 +0100

    test husky

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: 'airbnb-base',
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: [
+                '.eslintrc.{js,cjs}',
+            ],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    rules: {
+        'eol-last': 0,
+        'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
+        'linebreak-style': 0,
+        indent: ['error', 4, { SwitchCase: 1 }],
+        'no-console': 'off',
+    },
+};
